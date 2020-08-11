ace9bfe3bfd0c5213657df41a5a4577fa8a00e21
kogayama <kogayama0130@gmail.com>
2020-08-11 22:22:50 +0900

    edited user management of server

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'messages/index'
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   root "messages#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:edit, :update]
 end
