daab6e92c719703b9fafadfa151bce0cd033a637
kogayama <kogayama0130@gmail.com>
2020-08-13 10:46:52 +0900

    prepare ajax for events

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -18,11 +18,12 @@ class MessagesController < ApplicationController
   end
 
   private
-  def set_group
-    @group = Group.find(params[:group_id])
-  end
 
   def message_params
     params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
+  def set_group
+    @group = Group.find(params[:group_id])
   end
 end
